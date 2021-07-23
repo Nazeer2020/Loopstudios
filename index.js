2012491bcb937c171c58d54dd0e9d34846d8ccf8
Nazeer2020 <nazeer.haidari@gmail.com>
2021-07-23 19:56:28 +0430

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+$(".bars").click(()=>{
+    $(".fa-bars , .fa-times").toggleClass("fa-bars fa-times")
+    $(".mobile-menu , .show-menu").toggleClass("mobile-menu show-menu")
+    $(".animate__fadeInLeft , .animate__fadeInRight").toggleClass("animate__fadeInLeft animate__fadeInRight")
+})
+
+
+// // <i class="fal fa-times"></i>
+
+// animate__fadeInLeft
+// animate__fadeInRight
+
